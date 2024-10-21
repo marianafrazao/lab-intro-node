372c48d5297a4f168f04198ab26fb8438ad1f277
Mariana Frazão <mariana.franco.frazao@gmail.com>
2024-10-21 16:00:17 +0100

    done - mariana frazao

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b);
+    this.length++
+    return this.items
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= this.length){
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    } 
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...(this.items))
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...(this.items));
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0;
+    for (let i=0; i<this.length; i++){
+      sum += this.items[i];
+    }
+    return sum
+  }
 
-  avg() {}
+  avg() {
+    if (this.length > 0) {
+      let sum = 0;
+      for (let i=0; i<this.length; i++){
+        sum += this.items[i];
+      }
+    return sum/this.length;
+  } else if (this.length === 0) {
+    throw new Error("EmptySortedList");
+  }
+}
 }
 
 module.exports = SortedList;
+
